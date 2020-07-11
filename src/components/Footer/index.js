98d23b1f0e3218c8b1b26ffb54286710e96872fe
Srikanth J <jnsrikanth@yahoo.com>
2020-07-11 19:52:41 +0530

    Updated Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -52,8 +52,7 @@ const Footer = () => (
           <Grid.Column width={7}>
             <Header as="h4">Footer Header</Header>
             <p>
-              Extra space for a call to action inside the footer that could help
-              re-engage users.
+              Your One Stop Shop for your Fashion and Home Decor Needs
             </p>
             <List horizontal style={{display: 'flex'}}>
               <List.Item
